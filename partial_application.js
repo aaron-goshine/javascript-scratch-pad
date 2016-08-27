fbc86c380c865d7c1462c217a10ab27948b7bca6
Goshine <greenlig@gmail.com>
2016-08-27 09:46:33 +0100

    feat(Partial Application exercise):

--- partial_application.js
+++ partial_application.js
@@ -80,6 +80,94 @@ var f = function (x, y, z) {
   return x * (y - z);
 };
 
+var sum = function (x, y) {
+  return x + y;
+};
+
 partialLeft(f, 2)(3, 4);
 partialRight(f, 2)(3, 4);
 partialLeft(f, undefined, 2)(3, 4);
+
+var increment = partialLeft(sum, 1);
+var cuberoot = partialRight(Math.pow, 1 / 3);
+String.first = partial(String.prototype.charAt, 0);
+String.last = partial(String.prototype.substr, -1, 1);
+
+/**
+ * Returns a function that composes f . g
+ *
+ * @name compose
+ * @function
+ * @param {function} f - function
+ * @param {function} g - function
+ * @returns {function} function to compose functions passed in as parameter
+ */
+function compose (f, g) {
+  return function () {
+    return f(g.apply(this, arguments));
+  };
+}
+
+var not = partialLeft(compose, function (x) {
+  return !x;
+});
+
+var even = function (x) {
+  return x % 2 === 0;
+};
+// Reduce an array to single value using a function
+var reduce = Array.prototype.reduce_ ? function (a, f, initialValue) {
+  if (initialValue) {
+    return a.reduce(f, initialValue);
+  }
+  return a.reduce(f);
+} : function (a, f, initialValue) {
+  var i = 0;
+  var len = a.length;
+  var accumulator;
+  if (initialValue) accumulator = initialValue;
+  if (len === 0) throw TypeError();
+  while (i < len) {
+    if (i in a) {
+      accumulator = a[i++];
+      break;
+    }
+    i++;
+  }
+  if (i === len) throw TypeError();
+
+  while (i < len) {
+    accumulator = f(accumulator, a[i]);
+    i++;
+  }
+  return accumulator;
+};
+
+// Array map implementation
+var map = Array.prototype.map_ ? function (a, f) {
+  return a.map(f);
+} : function (a, f) {
+  var results = [];
+  var len = a.length;
+  for (var i = 0; i < len; i++) {
+    if (i in a) results[i] = f(i);
+  }
+  return results;
+};
+
+var odd = not(even);
+var isNumber = not(isNaN);
+
+var data = [1, 1, 3, 5, 5];
+var product = function (x, y) {
+  return x * y;
+};
+var neg = partial(product, -1);
+var square = partial(Math.pow, undefined, 2);
+var sqrt = partial(Math.pow, undefined, 0.5);
+var reciprocal = partial(Math.pow, undefined, -1);
+
+var mean = product(reduce(data, sum), reciprocal(data.length));
+var stddev = sqrt(product(reduce(map(data, compose(square, partial(sum, neg(mean)))), sum),
+                          reciprocal(sum(data.length, -1))));
+
