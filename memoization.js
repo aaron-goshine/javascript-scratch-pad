1a0125fbb0ffd5475b0b9ace592d8bdf42abd2a2
Goshine <greenlig@gmail.com>
2016-08-28 10:37:05 +0100

    feat(Memoize): Implemented Common demominator

--- memoization.js
+++ memoization.js
@@ -18,3 +18,29 @@ function memoize (f) {
     return cache[key];
   };
 }
+
+/**
+ * Computes the common demoninator off two numbers
+ *
+ * @name gcd
+ * @function
+ * @param {number} a - first paramiter as a number
+ * @param {number} b - the second paramiter as a number
+ * @returns {number} the common demoninator of the paramiter
+ */
+function gcd (a, b) {
+  var t;
+  if (a < b) {
+    t = b;
+    b = a;
+    a = t;
+    while (b !== 0) {
+      t = b;
+      b = a % b;
+      a = t;
+    }
+    return a;
+  }
+}
+
+
